9d4289d1ab72ea8251d67ede34fa3bba77270813
ItsSven64 <svendewit1707@gmail.com>
2024-02-01 21:19:25 +0100

    Modified old UDP client
    UDP -> TCP

--- Webserver/ClienttestTCP.py
+++ Webserver/ClienttestTCP.py
@@ -1,11 +1,68 @@
 import socket
+import asyncio
+from time import sleep
 
-HOST = "localhost"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+msgFromClient       = "Hello TCP Server"
+bytesToSend         = str.encode(msgFromClient)
+serverAddressPort   = ("162.168.2.26", 65432)
+bufferSize          = 8192
+closing_byte        = b'\x00'
+req_msg             = b"/req"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+print("These are the parameters:")
+print("Address + Port {}".format(serverAddressPort))
+print("Buffer size: " + str(bufferSize))
+print("Closing byte: " + str(closing_byte))
+print("Request byte: " + str(req_msg)) 
 
-print(f"Received {data!r}")
+# Create a UDP socket at client side
+TCP_Socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+TCP_Socket.settimeout(1)
+# Send to server using created UDP socket
+
+async def recv_tcp():
+    msg = ''
+    while (msg != closing_byte):
+        try:
+            msgFromServer= await loop.run_in_executor(None, lambda: TCP_Socket.recv(bufferSize))
+        except TimeoutError:
+            break
+        msg = "{}".format(msgFromServer[0])
+        print(msg)
+
+async def send_tcp():
+        send = input("What do you want to send? ")
+        match (send):
+            case 'req log':
+                #See COMMS SP for info
+                try:
+                    n_lines = int(input("How many lines? (0=*) "))
+                    if (n_lines == 0):
+                        print("Requesting entire log!")
+                        data = 255
+                    else:
+                        data = 256-n_lines+1
+                        #data = 256 - x
+                        #x = 256 - data
+                    TCP_Socket.sendall(req_msg, serverAddressPort) 
+                    sleep(1)
+                    TCP_Socket.sendall(data.to_bytes(1), serverAddressPort)  
+                except ValueError:
+                    print("Invalid input, please try again!")
+                
+                  
+
+            case _: 
+                TCP_Socket.sendall(str.encode(send), serverAddressPort)
+         
+        await asyncio.create_task(recv_tcp())     
+
+async def main():
+        tasks = [recv_tcp(), send_tcp()]
+        await asyncio.gather(*tasks)
+
+if __name__ == "__main__":
+    
+    while(True):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
--- Webserver/backendTCP.py
+++ Webserver/backendTCP.py
@@ -1,10 +1,52 @@
-import socket
+import socket as sock
+import datetime
 
-HOST    = "localhost"
+HOST    = "192.168.2.26"
 PORT    = 65432
 
+today = datetime.date.today()
+filename = r"C:\Users\svend\Desktop\Programming\Python\SERVER" +r"\Logs -"+ str(today) + ".txt"
+log = open(filename, "a")
+
+def line_prepender(filename, line):
+    #prepends the line to the file
+    #arguments are self-explanatory
+    #courtesy of eyquem on StackOverflow
+    log.close()
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(line.rstrip('\r\n') + '\n' + content)
+
+'''
+def send_logs(IP, n_lines=0):
+    #sends the logs file to the user
+    #IP = target IP of client
+    #n_lines = the number of lines that will be sent (counting from the bottom) (-1 = the entire file)
+    global log
+    print("sending logs!")
+    log.close()
+    log = open(filename, "r")
+    if (n_lines == 0):
+        while True:
+            line = log.readline()
+            UDPServerSocket.sendto(str.encode(line), IP)
+            if not line:
+                break
+    else:
+        for i in range(n_lines):
+            x = log.readline()
+            UDPServerSocket.sendto(str.encode(x), IP)
+            if not x:
+                break
+    UDPServerSocket.sendto(closing_byte, IP)
+    log.close()
+    log = open(filename, "a")
+    print("done!")
+'''
+
 while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    with sock.socket(sock.AF_INET, sock.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
         conn, addr = s.accept()
@@ -15,3 +57,6 @@ while True:
                 if not data:
                     break
                 conn.sendall(data)
+                print(data)
+                data = data.decode() + "\n"
+                line_prepender(filename, data)
